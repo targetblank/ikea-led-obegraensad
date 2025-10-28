b8549b8ba61e86ee87602dc49e6db2a827fc4cdb
Andreas Bühl <code@abuehl.de>
2025-10-28 10:27:43 +0100

    remove conditional rendering from TickingClockPlugin

--- include/plugins/TickingClockPlugin.h
+++ include/plugins/TickingClockPlugin.h
@@ -7,10 +7,6 @@ class TickingClockPlugin : public Plugin
 private:
     struct tm timeinfo;
 
-    int previousMinutes;
-    int previousHour;
-    int previousSecond;
-
 public:
     void setup() override;
     void loop() override;
--- src/plugins/TickingClockPlugin.cpp
+++ src/plugins/TickingClockPlugin.cpp
@@ -2,15 +2,20 @@
 
 void TickingClockPlugin::setup()
 {
-  previousMinutes = -1;
-  previousHour = -1;
+  // loading screen
+  Screen.setPixel(4, 7, 1);
+  Screen.setPixel(5, 7, 1);
+  Screen.setPixel(7, 7, 1);
+  Screen.setPixel(8, 7, 1);
+  Screen.setPixel(10, 7, 1);
+  Screen.setPixel(11, 7, 1);
 }
 
 void TickingClockPlugin::loop()
 {
   if (getLocalTime(&timeinfo))
   {
-
+    // Always redraw - swapBuffers() will skip copy if unchanged (memcmp optimization)
     if ((timeinfo.tm_hour * 60 + timeinfo.tm_min) < 6 * 60 + 30 ||
         (timeinfo.tm_hour * 60 + timeinfo.tm_min) > 22 * 60 + 30) // only between 6:30 and 22:30
     {
@@ -18,33 +23,24 @@ void TickingClockPlugin::loop()
       return;
     }
 
-    if (previousHour != timeinfo.tm_hour || previousMinutes != timeinfo.tm_min)
-    {
+    std::vector<int> hh = {(timeinfo.tm_hour - timeinfo.tm_hour % 10) / 10, timeinfo.tm_hour % 10};
+    std::vector<int> mm = {(timeinfo.tm_min - timeinfo.tm_min % 10) / 10, timeinfo.tm_min % 10};
 
-      std::vector<int> hh = {(timeinfo.tm_hour - timeinfo.tm_hour % 10) / 10, timeinfo.tm_hour % 10};
-      std::vector<int> mm = {(timeinfo.tm_min - timeinfo.tm_min % 10) / 10, timeinfo.tm_min % 10};
+    Screen.clear();
 
-      Screen.clear();
+    // Draw hour digits (rows 0-7)
+    Screen.drawCharacter(2, 0, Screen.readBytes(fonts[1].data[hh[0]]), 8, Screen.getCurrentBrightness());
+    Screen.drawCharacter(9, 0, Screen.readBytes(fonts[1].data[hh[1]]), 8, Screen.getCurrentBrightness());
 
-      Screen.drawCharacter(2, 0, Screen.readBytes(fonts[1].data[hh[0]]), 8, Screen.getCurrentBrightness());
-      Screen.drawCharacter(9, 0, Screen.readBytes(fonts[1].data[hh[1]]), 8, Screen.getCurrentBrightness());
-      Screen.drawCharacter(2, 9, Screen.readBytes(fonts[1].data[mm[0]]), 8, Screen.getCurrentBrightness());
-      Screen.drawCharacter(9, 9, Screen.readBytes(fonts[1].data[mm[1]]), 8, Screen.getCurrentBrightness());
-      previousMinutes = timeinfo.tm_min;
-      previousHour = timeinfo.tm_hour;
-    }
-    if (previousSecond != timeinfo.tm_sec)
-    {
-      // clear second lane
-      Screen.clearRect(0, 7, 16, 2);
-      // alternating second pixel
-      if ((timeinfo.tm_sec * 32 / 60) % 2 == 0)
-        Screen.setPixel(timeinfo.tm_sec * 16 / 60, 7, 1, Screen.getCurrentBrightness());
-      else
-        Screen.setPixel(timeinfo.tm_sec * 16 / 60, 8, 1, Screen.getCurrentBrightness());
+    // Draw minute digits (rows 9-16)
+    Screen.drawCharacter(2, 9, Screen.readBytes(fonts[1].data[mm[0]]), 8, Screen.getCurrentBrightness());
+    Screen.drawCharacter(9, 9, Screen.readBytes(fonts[1].data[mm[1]]), 8, Screen.getCurrentBrightness());
 
-      previousSecond = timeinfo.tm_sec;
-    }
+    // Draw second indicator (rows 7-8)
+    if ((timeinfo.tm_sec * 32 / 60) % 2 == 0)
+      Screen.setPixel(timeinfo.tm_sec * 16 / 60, 7, 1, Screen.getCurrentBrightness());
+    else
+      Screen.setPixel(timeinfo.tm_sec * 16 / 60, 8, 1, Screen.getCurrentBrightness());
   }
 }
 
